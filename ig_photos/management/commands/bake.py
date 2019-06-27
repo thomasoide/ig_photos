fcc12985e5c687dd43dda581386d01e0e4a19038
Thomas Oide <thomas.t.oide@gmail.com>
2019-06-27 12:06:17 -0500

    fixed management command

--- ig_photos/management/commands/bake.py
+++ ig_photos/management/commands/bake.py
@@ -14,25 +14,31 @@ class Command(BaseCommand):
     """
     def handle(self, *args, **kwargs):
         bucketName = "static.startribune.com"
+        # bucketName = "strib-ig-photos"
 
         outputName = "assets/features/thomas/ig_photos.json"
 
         Key1 = "assets/features/thomas/ig_photos.json"
         output1 = "assets/features/thomas/old_ig_photos.json"
 
+        local_path = "old_ig_photos.json"
+
         filename = "ig_photos.json"
         old_filename = "old_ig_photos.json"
         # url = "http://localhost:8000/api/ig_photos/photo/?format=json"
         url = "http://ec2-13-57-84-236.us-west-1.compute.amazonaws.com/ig-photos/api/ig_photos/photo/?format=json"
 
-        s3 = boto3.resource('s3')
-        try:
-            s3.Bucket(bucketName).download_file(Key1, output1)
-        except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] == "404":
-                print("The object does not exist.")
-            else:
-                raise
+        # s3 = boto3.resource('s3')
+        # try:
+        #     s3.Bucket(bucketName).download_file(Key1)
+        # except botocore.exceptions.ClientError as e:
+        #     if e.response['Error']['Code'] == "404":
+        #         print("The object does not exist.")
+        #     else:
+        #         raise
+
+        s3 = boto3.client('s3')
+        s3.download_file(bucketName, Key1, local_path)
 
         self.stdout.write('Baking database to JSON...', ending="\n")
         resp = requests.get(url=url,)
